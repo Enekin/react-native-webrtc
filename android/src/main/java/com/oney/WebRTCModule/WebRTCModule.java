b4e799c1d3b546291c1f311bc8bf3a7945885f4c
Pierre Carion <pierre@rabb.it>
2019-03-12 17:52:38 -0700

    manually apply commit: https://github.com/oney/react-native-webrtc/commit/8def7d0dc9370934a4d56770d63d742c2a203fea for rn 0.58

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -15,6 +15,7 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.ArrayList;
@@ -30,6 +31,7 @@ import org.webrtc.audio.JavaAudioDeviceModuleRabbit.AudioTrackParamsChangedCallb
 
 import org.webrtc.*;
 
+@ReactModule(name = "WebRTCModule")
 public class WebRTCModule extends ReactContextBaseJavaModule {
     static final String TAG = WebRTCModule.class.getCanonicalName();
 
